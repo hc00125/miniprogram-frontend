da946c2b79e22e4e0dd7920bdaa24f3ca1e51cf6
Hu Chen <164363459+hc00125@users.noreply.github.com>
2026-07-10 16:00:18 +0800

    Remove virtual payment monkey patch

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
-import { installVirtualPaymentBridge } from '@/utils/virtual-payment'
 
 export function createApp() {
-  installVirtualPaymentBridge()
   const app = createSSRApp(App)
   return { app }
 }
