3ffc54ad301322269010e0a21278679767a83fd9
Hu Chen <164363459+hc00125@users.noreply.github.com>
2026-08-07 00:30:50 +0800

    Enable iOS order alert sound

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+import { configureOrderAlertAudioOptions } from '@/utils/audioOptions'
 
 export function createApp() {
+  configureOrderAlertAudioOptions()
   const app = createSSRApp(App)
   return { app }
 }
